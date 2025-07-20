a486fa2989a16d9f958f519fa8c994f5632690e5
lorenaurbieta <lorenaurbieta39@gmail.com>
2025-07-20 11:53:52 -0300

    Create funciones.py

---
+++ clase3/funciones.py
@@ -0,0 +1,11 @@
+'''
+Ejemplo de funciones en Python
+'''
+a=2
+b=3
+def sumar(a, b):
+    return a + b  # Definición de la función sumar
+print("Resultado de la suma:", sumar(a, b))  # Imprimir el resultado de la suma   
+def restar(a, b):
+    return a - b    # Definición de la función restar
+print("Resultado de la resta:", restar(a, b))  # Imprimir el resultado de la resta           
