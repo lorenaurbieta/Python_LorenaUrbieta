8d3fd612300e00e739f69376fc9f7489b74b5b60
lorenaurbieta <lorenaurbieta39@gmail.com>
2025-07-20 11:31:27 -0300

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,21 @@
+print("Hola mundo desde la UNIDA")
+
+entero=10
+flotante=10.5
+cadena="Hola mundo"
+booleano=True
+print(entero,flotante,cadena,booleano)
+print(type(entero),type(flotante),type(cadena),type(booleano))
+
+print("Sumando dos numeros")
+# Operaciones basicas con variables como Suma, Resta, Multiplicacion, Division
+a=10
+b=20
+print("Suma de dos numeros",a+b)
+print("Restando dos numeros")
+print("Resta de dos numeros",a-b)
+print("Multiplicando dos numeros")
+print("Multiplicacion de dos numeros",a*b)  
+b=2
+print("Dividiendo dos numeros")
+print("Division de dos numeros",a/b)
