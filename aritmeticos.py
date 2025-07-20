62b44db52b957ddac98b2818a0702f55cebf5e2c
lorenaurbieta <lorenaurbieta39@gmail.com>
2025-07-20 11:20:13 -0300

    Create aritmeticos.py

---
+++ aritmeticos.py
@@ -0,0 +1,25 @@
+'''
+Operaciones aritméticas
+suma, resta, multiplicacion y division de dos números
+c=a+b
+d=a-b
+e=a*b       
+f=a/b
+g=a%b
+h=a**b
+'''
+a = 15  # el usuario ingresa un numero si es que se le pide
+b = 25
+r=0 
+r = a + b
+print("Suma:", r)
+r = a - b 
+print("Resta:", r)       
+r = a * b
+print("Multiplicación:", r)
+r = a / b 
+print("División:", r)  
+r = a % b
+print("Módulo:", r)
+r = a ** b  
+print("Exponente:", r)
