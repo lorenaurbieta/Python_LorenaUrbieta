22599c5f8f980aedb6de88388d007f2c9e74492f
lorenaurbieta <lorenaurbieta39@gmail.com>
2025-07-20 11:30:30 -0300

    Create operdores.py

---
+++ operdores.py
@@ -0,0 +1,22 @@
+'''
+a == b
+a != b
+a < b
+a > b
+a <= b
+a >= b
+# Operadores de comparacion
+'''
+a=17 
+b=18
+r=a == b
+r=a != b
+print(r)
+r=a > b
+print("Es menor",r)  
+r=a < b
+print("Es Mayor",r) 
+r=a <= b
+print(r)
+r=a >= b
+print(r)
