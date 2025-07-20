dad76077c3af305856532256ace96de41245c003
lorenaurbieta <lorenaurbieta39@gmail.com>
2025-07-20 11:28:11 -0300

    Create entrada.py

---
+++ entrada.py
@@ -0,0 +1,8 @@
+'''
+Entrada de datos por teclado
+'''
+cadena = input("Ingrese nombre de su proyecto: ")
+print(f"El nombre de su proyecto es: {cadena}")
+
+numero = int(input("¿Que version es? "))
+print(f"La version de su proyecto es: {numero+1}")
